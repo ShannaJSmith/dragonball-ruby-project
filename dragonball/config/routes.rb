aa888a5ca3d63a506dabcb77e7303e35b4b6680b
ShannaJSmith <shanna_smith987@hotmail.com>
2022-03-06 23:46:21 +0000

    generate routes

--- dragonball/config/routes.rb
+++ dragonball/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "login", to: "users#index"
+  # get "login", controller: 'users, action: :index'
+
+  resources :characters
+  # resources :characters, only: [:index, :show] # if it reaches 4+ do the alternative
+  # resources :characters, except: [:destroy, :create, :update]
 end
