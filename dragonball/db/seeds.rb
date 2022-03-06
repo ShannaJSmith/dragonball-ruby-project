4f008e7029eb5e71da23169635f52c5c14d77f90
ShannaJSmith <shanna_smith987@hotmail.com>
2022-03-06 22:54:39 +0000

    add notes to readme and generate seeds

--- dragonball/db/seeds.rb
+++ dragonball/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "starting the seeds..."
+
+puts "creating the characters"
+
+10.times do
+  Character.create(
+  name: Faker::JapaneseMedia::DragonBall.character
+)
+end
+
+puts "retrieving characters"
+
+characters = Character.all #returns an array of objs
+
+puts "creating the planets"
+
+50.times do
+  Planet.create(
+    name: Faker::JapaneseMedia::DragonBall.planet,
+    race: Faker::JapaneseMedia::DragonBall.race,
+    character: characters.sample
+  )
+end
+
+puts "owari!"
