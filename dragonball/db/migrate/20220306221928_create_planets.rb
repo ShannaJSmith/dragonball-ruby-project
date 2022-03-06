444026f6d901d817c34dd9cc869857da7c38eb6c
ShannaJSmith <shanna_smith987@hotmail.com>
2022-03-06 22:34:53 +0000

    add keys to migrate files

--- dragonball/db/migrate/20220306221928_create_planets.rb
+++ dragonball/db/migrate/20220306221928_create_planets.rb
@@ -1,6 +1,10 @@
 class CreatePlanets < ActiveRecord::Migration[6.1]
   def change
     create_table :planets do |t|
+      t.string :name
+      t.string :race
+
+      t:references :character, index: true, foreign_key: true
 
       t.timestamps
     end
